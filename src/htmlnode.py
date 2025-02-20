f4dce8a9812638eca1d28619ad31133c38961515
harrisb002 <harrisbe002@gmail.com>
2025-02-20 05:08:53 -0800

    implementing LeafNode & converting to html

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,21 +1,26 @@
+from textnode import TextType
+
+
 # All HTMLNodes are assumed to have either a value or children
 # An HTMLNode without a tag will just render as raw text
 # An HTMLNode without props simply won't have any attributes
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag  # "string tag name -> 'p', 'a'..."
-        self.value = value  # string rep. the value of tag
+        self.value = value  # string rep. the value of tag (Also can be TextNode)
         self.children = children  # list of HTMLNode obj. children
         self.props = (
             props  # dict. rep. attributes of the tag -> "https://www.google.com"
         )
 
     # Children class shall overide this method
-    # def to_html(self):
-    #     raise NotImplementedError()
+    def to_html(self):
+        raise NotImplementedError()
 
     # Returns the string that represents the HTML Attributes of the Node
     def props_to_html(self):
+        if not self.props:
+            return ""
         html_props = ""
         for key, value in self.props.items():
             html_props += f" {key}={value}"
@@ -24,3 +29,20 @@ class HTMLNode:
     # Overriding print implementation
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        if value is None:
+            raise ValueError("All Leaf nodes must have a value.")
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    # Renders a leaf node as an HTML string (by returning a string).
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All Leaf nodes must have a value.")
+        if self.tag is None:
+            return self.value
+
+        props = self.props_to_html()
+        return f"<{self.tag}{props}>{self.value}</{self.tag}>"
--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    # node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    # print(node)
 
-    text_node = TextNode("Click me", "link", "https://www.boot.dev")
-    html_node = HTMLNode(
-        tag="a",
-        value=text_node.text,
-        props={"href": "https://www.google.com", "target": "_blank"},
-    )
-    print(html_node.props_to_html())
+    # text_node = TextNode("Click me", "link", "https://www.boot.dev")
+    # html_node = HTMLNode(
+    #     tag="a",
+    #     value=text_node.text,
+    #     props={"href": "https://www.google.com", "target": "_blank"},
+    # )
+    # print(html_node.props_to_html())
+
+    TestleafNode = LeafNode("p", "This is a paragraph of text.")
+    TestleafNode2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+
+    print(TestleafNode.to_html())  # <p>This is a paragraph of text.</p>
+    print(TestleafNode2.to_html())  # <a href="https://www.google.com">Click me!</a>
 
 
 if __name__ == "__main__":
