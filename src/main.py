9a7d5001d5cc32044b35a027d79efc959844d9d3
harrisb002 <harrisbe002@gmail.com>
2025-02-20 02:14:20 -0800

    Printing TextNode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import *
+
+
+def main():
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
