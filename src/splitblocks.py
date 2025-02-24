188d6d86a46d30b30145404f1706ea136d3b396f
harrisb002 <harrisbe@sonoma.edu>
2025-02-24 05:06:48 -0800

    Filtering markdown blocks

---
+++ src/splitblocks.py
@@ -0,0 +1,11 @@
+# Takes a raw Markdown string (representing a full document) as input
+# Returns a list of "block" strings.
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
