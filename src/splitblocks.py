622dab61837b9462a073fdb52d9c4e53506ad439
harrisb002 <harrisbe@sonoma.edu>
2025-02-24 14:41:46 -0800

    Adding split blocks

--- src/splitblocks.py
+++ src/splitblocks.py
@@ -1,3 +1,57 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading" 
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+# Takes a single block of markdown text as input 
+# Returns the BlockType representing the type of block it is. 
+# Assumes all leading and trailing whitespace was already stripped (done in markdown_to_blocks function).
+def block_to_block_type(block):
+    ''' 
+    Rules:
+        Headings start with 1-6 # characters, followed by a space and then the heading text.
+        Code blocks must start with 3 backticks and end with 3 backticks.
+        Every line in a quote block must start with a > character.
+        Every line in an unordered list block must start with a * or - character, followed by a space.
+        Every line in an ordered list block must start with a number followed by a . character and a space. The number must start at 1 and increment by 1 for each line.
+        If none of the above conditions are met, the block is a normal paragraph. 
+    '''
+    lines = block.split("\n")
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
+
+
+
 # Takes a raw Markdown string (representing a full document) as input
 # Returns a list of "block" strings.
 def markdown_to_blocks(markdown):
